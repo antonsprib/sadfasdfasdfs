84d655a3e3b126f551ea6b8592dfe67ee7337922
MMM <mmaksmas@gmail.com>
2018-12-06 20:21:52 +0200

    aUTHORITY admin

--- src/main/java/lv/helloit/test/CustomAuthenticationProvider.java
+++ src/main/java/lv/helloit/test/CustomAuthenticationProvider.java
@@ -2,19 +2,24 @@ package lv.helloit.test;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.jaas.JaasAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.AuthProvider;
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return new UsernamePasswordAuthenticationToken("User", "password", new ArrayList<>());
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ADMIN"));
+
+        return new UsernamePasswordAuthenticationToken("User", "password", authorities);
     }
 
     @Override
